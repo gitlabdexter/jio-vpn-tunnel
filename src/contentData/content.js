113aba56aa467ea2b438c66d27671aae7afa0136
gitlabdexter <gitlabdexter@gmail.com>
2025-10-02 22:18:39 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,46 +1,56 @@
 const productData = [
     {
       id: 1,
-      name: "Jio Tunnel Vn",
+      name: "JIO TUNNEL VPN",
       image:"https://i.ibb.co/jPnXFqj5/icon-icon.png",
       productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY"],
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
       rating: 5,
       timeLeft: 24,
       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app-release.apk" 
-     //  },
-   // {
-    //  id: 2,
-    //  name: "180G HIGH SPEED PRO",
-    //  image:"https://i.ibb.co/Gv6jGVQx/icon-icon.png",
-    //  productType: "openBackHeadphones",
-    //  description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-     // rating: 5,
-    //  timeLeft: 27,
-    //  appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/demo-release.apk"
-   // },
-    //{
-   //   id: 3,
-   //   name: "IPHONE SPEED PRO",
-    //  image:
-    //    "https://i.ibb.co/z7pfLQc/icon-icon.png",
-   //   productType: "openBackHeadphones",
-   //   description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-   //   rating: 5,
-    //  timeLeft: 24,
-   //   appLink: "https://panelvip.co.uk/iphone.apk"
+       },
+    {
+      id: 2,
+      name: "DILSHAD VPN",
+      image:"https://i.ibb.co/gMGWG4Gd/dishad-vpn.jpg",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+      rating: 5,
+     timeLeft: 27,
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DIL_SHAD_VPN.apk"
+    },
+    {
+      id: 3,
+      name: "JIO SOCIAL VPN",
+      image:"https://i.ibb.co/8ymLfzV/jio-social-vpn.jpg",
+     productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY""],
+     rating: 5,
+      timeLeft: 24,
+     appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/JIO_SOCIAL_VPN.apk"
 
-    }
-    // {
-    //   id: 4,
-    //   name: "SECURE TUNNEL",
-    //   image:
-    //     "https://i.postimg.cc/wMW7LWFC/keyhole-958517.png",
-    //   productType: "openBackHeadphones",
-    //   description: ["OVPN", "UDP", "V2RAY"],
-    //   rating: 5,
-    //   timeLeft: 24,
-    //   appLink: "https://tinyurl.com/StarLinkVPNnew-Apk"
-    // }
+    },
+     {
+       id: 4,
+       name: "DU CARD VPN",
+       image:
+         "https://i.ibb.co/qTsHqk0/du-card.jpg",
+       productType: "openBackHeadphones",
+       description: ["OVPN", "UDP", "V2RAY"],
+       rating: 5,
+       timeLeft: 24,
+       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DU_CARD_VPN.apk"
+	          },
+    {
+      id: 5,
+      name: "ANSARI VPN",
+      image:"https://i.ibb.co/nq4PfRTF/a.jpg",
+      productType: "openBackHeadphones",
+      description: ["OVPN", "UDP", "V2RAY""],
+      rating: 5,
+     timeLeft: 27,
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ANSARI_VPN_1.2_APKPure.xapk"
+   
+     }
   ];
   export default productData;
