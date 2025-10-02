d1894f3aaef997ccbe0483623c613960fdaa168e
gitlabdexter <gitlabdexter@gmail.com>
2025-10-02 22:49:38 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,61 +1,59 @@
 const productData = [
-    {
-      id: 1,
-      name: "JIO TUNNEL VPN",
-      image:"https://i.ibb.co/jPnXFqj5/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 24,
-	  downloads: 0,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app-release.apk" 
-       },
-    {
-      id: 2,
-      name: "DILSHAD VPN",
-      image:"https://i.ibb.co/gMGWG4Gd/dishad-vpn.jpg",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 27,
-	  downloads: 0,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DIL_SHAD_VPN.apk"
-    },
-    {
-      id: 3,
-      name: "JIO SOCIAL VPN",
-      image:"https://i.ibb.co/8ymLfzV/jio-social-vpn.jpg",
-     productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY""],
-     rating: 5,
-      timeLeft: 24,
-	  downloads: 0,
-     appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/JIO_SOCIAL_VPN.apk"
+  {
+    id: 1,
+    name: "JIO TUNNEL VPN",
+    image: "https://i.ibb.co/jPnXFqj5/icon-icon.png",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+    rating: 5,
+    timeLeft: 24,
+    downloads: 0,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app-release.apk"
+  },
+  {
+    id: 2,
+    name: "DILSHAD VPN",
+    image: "https://i.ibb.co/gMGWG4Gd/dishad-vpn.jpg",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+    rating: 5,
+    timeLeft: 27,
+    downloads: 0,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DIL_SHAD_VPN.apk"
+  },
+  {
+    id: 3,
+    name: "JIO SOCIAL VPN",
+    image: "https://i.ibb.co/8ymLfzV/jio-social-vpn.jpg",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY"],   // ✅ fixed
+    rating: 5,
+    timeLeft: 24,
+    downloads: 0,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/JIO_SOCIAL_VPN.apk"
+  },
+  {
+    id: 4,
+    name: "DU CARD VPN",
+    image: "https://i.ibb.co/qTsHqk0/du-card.jpg",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY"],
+    rating: 5,
+    timeLeft: 24,
+    downloads: 0,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DU_CARD_VPN.apk"
+  },
+  {
+    id: 5,
+    name: "ANSARI VPN",
+    image: "https://i.ibb.co/nq4PfRTF/a.jpg",
+    productType: "openBackHeadphones",
+    description: ["OVPN", "UDP", "V2RAY"],   // ✅ fixed
+    rating: 5,
+    timeLeft: 27,
+    downloads: 0,
+    appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ANSARI_VPN_1.2_APKPure.xapk"
+  }
+];
 
-    },
-     {
-       id: 4,
-       name: "DU CARD VPN",
-       image:
-         "https://i.ibb.co/qTsHqk0/du-card.jpg",
-       productType: "openBackHeadphones",
-       description: ["OVPN", "UDP", "V2RAY"],
-       rating: 5,
-       timeLeft: 24,
-	   downloads: 0,
-       appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/DU_CARD_VPN.apk"
-	          },
-    {
-      id: 5,
-      name: "ANSARI VPN",
-      image:"https://i.ibb.co/nq4PfRTF/a.jpg",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY""],
-      rating: 5,
-     timeLeft: 27,
-	 downloads: 0,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/ANSARI_VPN_1.2_APKPure.xapk"
-   
-     }
-  ];
-  export default productData;
+export default productData;
