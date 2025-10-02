bb0170f0c0018770e935ad5295d82a0040ec5713
gitlabdexter <gitlabdexter@gmail.com>
2025-10-02 22:49:02 +0800

    Update products.js

--- src/components/products.js
+++ src/components/products.js
@@ -2,14 +2,11 @@ import { FaStar } from 'react-icons/fa';
 import { useState } from 'react';
 
 export function Products(props) {
-  // downloads state (start from props.downloads if provided)
   const [downloads, setDownloads] = useState(props.downloads || 0);
 
-  // handle click
   const handleDownload = () => {
-    setDownloads(downloads + 1);  // increase counter
-    // open APK link
-    window.open(props.appLink, '_blank');
+    setDownloads(downloads + 1);
+    window.open(props.appLink, '_blank'); // open APK in new tab
   };
 
   return (
@@ -18,6 +15,7 @@ export function Products(props) {
         <img src={props.image} alt='app-img' className='productImage' />
         <div className='productCard__content'>
           <h3 className='productName'>{props.name}</h3>
+
           <div className='displayStack__1'>
             <select className='productPrice'>
               {props.description.map((desOption, index) => (
@@ -26,6 +24,7 @@ export function Products(props) {
                 </option>
               ))}
             </select>
+
             <div className='productRating'>
               {[...Array(props.rating)].map((_, index) => (
                 <FaStar id={index + 1} key={index} />
