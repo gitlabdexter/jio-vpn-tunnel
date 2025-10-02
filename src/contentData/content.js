f303fac3cb8d9629b2277d819e8dfcf01076d771
gitlabdexter <gitlabdexter@gmail.com>
2025-10-02 18:50:18 +0800

    Update content.js

--- src/contentData/content.js
+++ src/contentData/content.js
@@ -1,23 +1,23 @@
 const productData = [
     {
       id: 1,
-      name: "Box 5g Speed Pro",
-      image:"https://i.ibb.co/1RF1Gfq/favicon.png",
+      name: "Jio Tunnel Vn",
+      image:"https://i.ibb.co/jPnXFqj5/icon-icon.png",
       productType: "openBackHeadphones",
       description: ["OVPN", "UDP", "V2RAY"],
       rating: 5,
       timeLeft: 24,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app.boxvpntunnel.dexbuild-DexBuild-V2-release.apk" 
-       },
-    {
-      id: 2,
-      name: "180G HIGH SPEED PRO",
-      image:"https://i.ibb.co/Gv6jGVQx/icon-icon.png",
-      productType: "openBackHeadphones",
-      description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
-      rating: 5,
-      timeLeft: 27,
-      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/demo-release.apk"
+      appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/app-release.apk" 
+     //  },
+   // {
+    //  id: 2,
+    //  name: "180G HIGH SPEED PRO",
+    //  image:"https://i.ibb.co/Gv6jGVQx/icon-icon.png",
+    //  productType: "openBackHeadphones",
+    //  description: ["OVPN", "UDP", "V2RAY", "SSH", "SLOWDNS"],
+     // rating: 5,
+    //  timeLeft: 27,
+    //  appLink: "https://gitlab.com/mtkdev1/dextereskalarte/-/raw/main/demo-release.apk"
    // },
     //{
    //   id: 3,
