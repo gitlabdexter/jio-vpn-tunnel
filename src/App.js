b1cd6f34355dadde2a9e7b0d670d4d7163313979
gitlabdexter <gitlabdexter@gmail.com>
2025-10-02 22:43:45 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,39 +1,39 @@
- import { Products } from './components/products';
- import contents from './contentData/content';
- import siteOptions from './contentData/siteOption';
- import CoverPage from './components/cover_page';
- import FooterDetails from './components/footer_details';
+import { Products } from './components/products';
+import contents from './contentData/content';
+import siteOptions from './contentData/siteOption';
+import CoverPage from './components/cover_page';
+import FooterDetails from './components/footer_details';
 
- export default function App() {
-     return(
-        <>
-        {siteOptions.map(siteOptions => (
-                    <CoverPage 
-                        key={siteOptions.id}
-                        image={siteOptions.image}
-                        siteName={siteOptions.siteName}
-                        description={siteOptions.description}
-                        siteLink={siteOptions.siteLink}
-                    />
-                ))}
-          <div className='AppContainer'>
-  {contents.map(content => (
-    <Products 
-      key={content.id}
-      image={content.image}
-      name={content.name}
-      description={content.description}
-      appLink={content.appLink}
-      timeLeft={content.timeLeft}
-      downloads={content.downloads}
-      rating={content.rating}
-    />
-  ))}
-</div>
+export default function App() {
+  return (
+    <>
+      {siteOptions.map(option => (
+        <CoverPage 
+          key={option.id}
+          image={option.image}
+          siteName={option.siteName}
+          description={option.description}
+          siteLink={option.siteLink}
+        />
+      ))}
 
-            <FooterDetails
-            key={siteOptions.id}
-            siteName={siteOptions.siteName}/>
-            </>
-     )
- }
+      <div className='AppContainer'>
+        {contents.map(content => (
+          <Products 
+            key={content.id}
+            image={content.image}
+            name={content.name}
+            description={content.description}
+            appLink={content.appLink}
+            timeLeft={content.timeLeft}
+            downloads={content.downloads}
+            rating={content.rating}
+          />
+        ))}
+      </div>
+
+      {/* If you want only one footer, show first */}
+      <FooterDetails siteName={siteOptions[0].siteName} />
+    </>
+  );
+}
